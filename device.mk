31b692924b8cb0568603ce2a323bfe4240d20591
Michael Bestas <mkbestas@gmail.com>
2025-07-24 21:14:59 +0300

    zuma: Drop kernel copy
    We are using prebuilt boot.img. If we weren't using it, we would be
    using the vendor/lineage kernel.mk rules to copy the kernel.
    
    Change-Id: Ib8ab9416b96900f008dbce9d9f41f298a1b05f30

--- device.mk
+++ device.mk
@@ -78,8 +78,6 @@ PRODUCT_SOONG_NAMESPACES += \
 	device/google/zuma \
 	device/google/zuma/powerstats
 
-LOCAL_KERNEL := $(TARGET_KERNEL_DIR)/Image.lz4
-
 # Set the environment variable to switch the Keymint HAL service to Rust
 TRUSTY_KEYMINT_IMPL := rust
 
@@ -313,7 +311,6 @@ PRODUCT_ENFORCE_PRODUCT_PARTITION_INTERFACE := true
 
 # Init files
 PRODUCT_COPY_FILES += \
-	$(LOCAL_KERNEL):kernel \
 	device/google/zuma/conf/init.zuma.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.zuma.usb.rc \
 	device/google/zuma/conf/ueventd.zuma.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc
 
