e17d707076bdef6e993ae5a36cc7fb0017daa376
Michael Bestas <mkbestas@lineageos.org>
2025-11-21 22:27:13 +0200

    Revert "zuma: Include Pixel experiments"
    This reverts commit 8f531bba37371fbe63e674c66a7b26fb68b13626.
    
    Reason for revert: No longer present in BP3A.
    
    Change-Id: I4dc49c3fec09c43583d4bf4409f2af1756dfc544

--- lineage_common.mk
+++ lineage_common.mk
@@ -20,9 +20,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PACKAGES += \
     EuiccSupportPixelOverlay
 
-# Experiments
-include device/google/gs-common/performance/experiments/experiments.mk
-
 # Google Assistant
 PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
 
