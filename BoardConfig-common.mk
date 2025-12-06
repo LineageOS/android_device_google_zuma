2c9727d8817f84b7a7899ab9dae9641b4302cad8
Michael Bestas <mkbestas@lineageos.org>
2025-12-06 10:42:25 +0200

    zuma: Import common cmdline from devices
    Change-Id: Ide25d370a938924c3963d7034e9c75eb16143c81

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -21,8 +21,9 @@ TARGET_CPU_VARIANT := cortex-a55
 BOARD_KERNEL_CMDLINE += earlycon=exynos4210,0x10870000 console=ttySAC0,115200 androidboot.console=ttySAC0 printk.devkmsg=on
 BOARD_KERNEL_CMDLINE += cma_sysfs.experimental=Y
 BOARD_KERNEL_CMDLINE += rcupdate.rcu_expedited=1 rcu_nocbs=all rcutree.enable_rcu_lazy
-BOARD_KERNEL_CMDLINE += swiotlb=1024
+BOARD_KERNEL_CMDLINE += swiotlb=noforce
 BOARD_KERNEL_CMDLINE += cgroup.memory=nokmem
+BOARD_KERNEL_CMDLINE += disable_dma32=on
 BOARD_KERNEL_CMDLINE += sysctl.kernel.sched_pelt_multiplier=4
 
 # Normal (non-_fullmte) builds should disable kasan
